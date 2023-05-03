4a8323075cb366e6b989b0664909510e48717c9d
Krzysztof Woś <krzysztof.wos@pm.me>
2023-05-03 14:50:01 +0000

    Implement particle collision

--- task02/main.cpp
+++ task02/main.cpp
@@ -56,13 +56,15 @@ void collide_particle_ball(
   //             no friction. You do not need to change the positions.
 
   // comment out the line below
-  p.velo -= 2.f * (p.velo - ball_velo).dot(plane_norm) * plane_norm;
+  // p.velo -= 2.f * (p.velo - ball_velo).dot(plane_norm) * plane_norm;
 
   // write a few lines of code to compute the velocity of ball and particle
   // please uncomment the lines below
-  // const Eigen::Vector2f impulse =
-  // p.velo +=
-  // ball_velo +=
+  const Eigen::Vector2f relative_velocity = p.velo - ball_velo;
+  float impulse_magnitude = 2.f * relative_velocity.dot(plane_norm) / (1.f / particle_mass + 1.f / ball_mass);
+  const Eigen::Vector2f impulse = impulse_magnitude * plane_norm;
+  p.velo -= impulse / particle_mass;
+  ball_velo += impulse / ball_mass;
 }
 
 /**
